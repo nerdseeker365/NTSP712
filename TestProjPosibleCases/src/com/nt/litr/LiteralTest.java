206a76b3e62442219d8e79d9fa64b6f1f7ebf2ce
admin <admin@DESKTOP-8KUS3QR>
2019-10-29 17:00:13 +0530

    java

---
+++ TestProjPosibleCases/src/com/nt/litr/LiteralTest.java
@@ -0,0 +1,17 @@
+package com.nt.litr;
+
+public class LiteralTest {
+//Any value that we store into a variable will be called as Literal...
+	public static void main(String[] args) {
+	//literals in java
+		int a=2;
+		long l=23L;
+		float f=34.00f;
+		double d=23.00D;
+		char c='a';
+		boolean b=true;
+		System.out.println(a+"\t"+l+"\t"+f+"\t"+d+"\t"+c+"\t"+b);
+
+	}
+
+}
