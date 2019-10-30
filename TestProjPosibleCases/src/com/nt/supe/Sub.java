088084c82b2ebca6871477af411b6240c968859e
admin <admin@DESKTOP-8KUS3QR>
2019-10-30 15:17:05 +0530

    test cases

---
+++ TestProjPosibleCases/src/com/nt/supe/Sub.java
@@ -0,0 +1,17 @@
+package com.nt.supe;
+
+public class Sub extends Test {
+//Inheritance relation is-a
+	//create child class obj access parent and child class members
+	//first prefer to child class it is not available next search in parent class
+	public void te(){
+		System.out.println("Sub.te()");
+	}
+
+	public static void main(String[] args) {
+		Sub s=new Sub();
+		s.t(7);//first search child class and next search super class 
+
+	}
+
+}
---
+++ TestProjPosibleCases/src/com/nt/supe/Test.java
@@ -0,0 +1,13 @@
+package com.nt.supe;
+
+public class Test {
+	public void  t(int x){
+		System.out.println("parent one param ");
+		te();
+	}
+	public void te() {
+		System.out.println("parent zero param ");
+	}
+	
+
+}
