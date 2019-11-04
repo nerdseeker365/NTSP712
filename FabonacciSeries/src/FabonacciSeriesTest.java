9d081e286f0aa2fd599d19373798817a8741acfb
admin <admin@DESKTOP-8KUS3QR>
2019-11-04 10:05:48 +0530

    FibonacciSeries

--- FabonacciSeries/src/FabonacciSeriesTest.java
+++ FabonacciSeries/src/FabonacciSeriesTest.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class FabonacciSeriesTest {
 //1,1,2,3,5,8,13,21,34,55..........
 	public static void main(String[] args) {
 		int i,n=1,n1=0;
-			for(i=1;i<=10;i++) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Value::");
+		int r=sc.nextInt();
+			for(i=1;i<=r;i++) {
 				int n3=n+n1;
 				System.out.print(n3+" ");
 				n=n1;
