08f0951d5406380acce0a93e7944cde806787491
sankar <karrasankar09@gmail.com>
2019-11-30 11:15:29 +0530

    Polymorphism

---
+++ Polymorphism/src/com/nt/test/Test.java
@@ -0,0 +1,16 @@
+package com.nt.test;
+//Constructor Overloading 
+public class Test {
+
+	public Test(){
+		
+	}
+	Test(int a){
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Polymorphism/src/com/nt/test/Test1.java
@@ -0,0 +1,13 @@
+package com.nt.test;
+
+public class Test1 extends Test{
+	
+	//public Test(){
+		
+	//}
+
+	public static void main(String[] args) {
+
+	}
+
+}
