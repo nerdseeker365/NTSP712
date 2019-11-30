dddfb604ea7d650d3a545d9c7b18904968d77caa
sankar <karrasankar09@gmail.com>
2019-11-30 11:11:01 +0530

    VOWELS AND CONSONANTS PRINTTING

---
+++ VowelsAndConsonents/src/com/nt/test/VCTest.java
@@ -0,0 +1,23 @@
+package com.nt.test;
+
+import java.util.Scanner;
+
+public class VCTest {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter String:: ");
+		String s=sc.nextLine();
+		for(int i=0;i<s.length();i++) {
+			char ch=s.charAt(i);
+			if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'|| ch==' ') {
+				System.out.print(" $");
+			}
+			else {
+				System.out.print("\n @");
+			}
+					}
+		sc.close();
+	}
+
+}
