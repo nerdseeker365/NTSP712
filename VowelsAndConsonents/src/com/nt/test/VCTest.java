70293ac96c15136a2833ae1333a97f76b3976a43
sankar <karrasankar09@gmail.com>
2019-11-30 11:13:50 +0530

    Vowels And Consonants Test

--- VowelsAndConsonents/src/com/nt/test/VCTest.java
+++ VowelsAndConsonents/src/com/nt/test/VCTest.java
@@ -1,9 +1,6 @@
 package com.nt.test;
-
 import java.util.Scanner;
-
 public class VCTest {
-
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter String:: ");
