6de159c181f130a863797ceb7b632245ffe491f5
sankar <karrasankar09@gmail.com>
2019-12-03 21:57:04 +0530

    charAt

---
+++ String_CharAt_Method/src/com/nt/test/CharAtTest1.java
@@ -0,0 +1,13 @@
+package com.nt.test;
+
+public class CharAtTest1 {
+
+	public static void main(String[] args) {
+			String s="DREAMBOY";
+			char c[]=s.toCharArray();
+			for(int i=0;i<c.length;i++) {
+				System.out.println(c[i]+"-1");
+			}
+	}
+
+}
